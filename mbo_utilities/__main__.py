c6f07fd372dfbad7e2ee52dbabe87d11791f3af6
Flynn <Flynnoconnell@gmail.com>
2025-01-15 10:33:09 -0500

    subclass scan to coerce to tzxy

--- mbo_utilities/__main__.py
+++ mbo_utilities/__main__.py
@@ -3,8 +3,52 @@ from pathlib import Path
 
 import numpy as np
 import fastplotlib as fpl
-import scanreader
-from mbo_utilities import get_metadata
+from scanreader.exceptions import PathnameError, FieldDimensionMismatch
+from scanreader.core import scans, expand_wildcard
+
+
+def read_scan(pathnames, dtype=np.int16, join_contiguous=False):
+    """ Reads a ScanImage scan. """
+    # Expand wildcards
+    filenames = expand_wildcard(pathnames)
+    if len(filenames) == 0:
+        error_msg = 'Pathname(s) {} do not match any files in disk.'.format(pathnames)
+        raise PathnameError(error_msg)
+
+    scan = ScanMultiROIReordered(join_contiguous=join_contiguous)
+
+    # Read metadata and data (lazy operation)
+    scan.read_data(filenames, dtype=dtype)
+
+    return scan
+
+
+class ScanMultiROIReordered(scans.ScanMultiROI):
+    """
+    A subclass of ScanMultiROI that ignores the num_fields dimension
+    and reorders the output to [time, z, x, y].
+    """
+
+    def __getitem__(self, key):
+        if not isinstance(key, tuple):
+            key = (key,)
+
+        # Call the parent class's __getitem__ with the reordered key
+        item = super().__getitem__((0, key[2], key[3], key[1], key[0]))
+        if item.ndim == 2:
+            return item
+        elif item.ndim == 3:
+            return np.transpose(item, (2, 0, 1))
+        else:
+            raise FieldDimensionMismatch('ScanMultiROIReordered.__getitem__')
+
+    @property
+    def shape(self):
+        return self.num_frames, self.num_channels, self.field_heights[0], self.field_widths[0]
+
+    @property
+    def ndim(self):
+        return 4
 
 
 def add_args(parser: argparse.ArgumentParser):
@@ -26,11 +70,9 @@ def add_args(parser: argparse.ArgumentParser):
     parser.add_argument('--version', action='store_true', help='Print the version of the package.')
     return parser
 
-def update_scan(scan):
-    pass
 
 def main():
-    parser = argparse.ArgumentParser(description="MBO Utilities")
+    parser = argparse.ArgumentParser(description="Preview a scanimage imaging session.")
     parser = add_args(parser)
     args = parser.parse_args()
 
@@ -41,14 +83,7 @@ def main():
         return
 
     files = [str(f) for f in Path(args.path).glob('*.tif*')]
-    metadata = get_metadata(files[0])
-
-    scan = scanreader.read_scan(files, join_contiguous=True)
-    scan[:]  # shape (600, 576, 30, 1730)
-
-    scan.ndim = 4
-    scan.shape = (600, 576, metadata['num_planes'], metadata['num_frames'])
-
+    scan = read_scan(files, join_contiguous=True)
     iw = fpl.ImageWidget(scan, histogram_widget=False)
     iw.show()
     fpl.loop.run()
@@ -56,8 +91,3 @@ def main():
 
 if __name__ == '__main__':
     main()
-
-
-
-
-
