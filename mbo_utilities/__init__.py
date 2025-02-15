a76f131c2540e3208958ad34c8a61a88ade8cb23
Flynn <Flynnoconnell@gmail.com>
2025-02-15 10:09:10 -0500

    don't expose wildcard/json serialization

--- mbo_utilities/__init__.py
+++ mbo_utilities/__init__.py
@@ -3,8 +3,6 @@ from .lcp_io import (
     stack_from_files,
     get_metadata,
     read_scan,
-    expand_wildcard,
-    make_json_serializable,
     is_raw_scanimage,
 )
 from .gui.widgets import run_gui
@@ -13,10 +11,10 @@ from . import _version
 __version__ = _version.get_versions()['version']
 
 __all__ = [
-    'get_metadata',
     'get_files_ext',
     'stack_from_files',
+    'get_metadata',
     'read_scan',
-    'run_gui',
     'is_raw_scanimage',
+    'run_gui',
 ]
