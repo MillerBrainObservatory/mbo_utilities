30deef1511ab5d17ae0459048f9e0e9aa420ce73
Flynn <Flynnoconnell@gmail.com>
2025-02-18 11:00:54 -0500

    add file_io funcs to namespace

--- mbo_utilities/__init__.py
+++ mbo_utilities/__init__.py
@@ -3,17 +3,32 @@ from .file_io import (
     stack_from_files,
     read_scan,
     ScanMultiROIReordered,
+    save_png,
+    save_mp4,
+    scans,
 )
 from .metadata import is_raw_scanimage, get_metadata
 from .gui.widgets import run_gui
+from .image import fix_scan_phase, return_scan_offset
 
 from . import _version
 __version__ = _version.get_versions()['version']
 
 __all__ = [
+    # gui
+    'run_gui',
+    # image processing
+    'fix_scan_phase',
+    'return_scan_offset',
+    # file_io
+    'ScanMultiROIReordered',
     'get_files',
     'stack_from_files',
     'read_scan',
-    'run_gui',
-    'ScanMultiROIReordered',
+    'save_png',
+    'save_mp4',
+    'scans',
+    # metadata
+    'is_raw_scanimage',
+    'get_metadata',
 ]
--- mbo_utilities/file_io.py
+++ mbo_utilities/file_io.py
@@ -125,25 +125,6 @@ def get_files(base_dir, str_contains="", max_depth=1, sort_ascending=True) -> li
     return [str(file) for file in files]
 
 
-def get_metrics_path(fname: Path) -> Path:
-    """
-    Get the path to the computed metrics file for a given data file.
-    Assumes the metrics file is to be stored in the same directory as the data file,
-    with the same name stem and a '_metrics.npz' suffix.
-
-    Parameters
-    ----------
-    fname : Path
-        The path to the input data file.
-
-    Returns
-    -------
-    metrics_path : Path
-        The path to the computed metrics file.
-    """
-    fname = Path(fname)
-    return fname.with_stem(fname.stem + '_metrics').with_suffix('.npz')
-
 
 def stack_from_files(files: list):
     """Stacks a list of TIFF files into a Dask array. Can be 3D Tyx or 4D Tzyx.
