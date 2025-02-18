9fd5920c738c99d890aecef79ee06a5c966a3aad
Flynn <Flynnoconnell@gmail.com>
2025-02-18 16:53:52 -0500

    finish chunked save_as, start demo

--- mbo_utilities/__init__.py
+++ mbo_utilities/__init__.py
@@ -11,12 +11,13 @@ from .assembly import save_as
 from .metadata import is_raw_scanimage, get_metadata
 from .gui.widgets import run_gui
 from .image import fix_scan_phase, return_scan_offset
-from .util import norm_minmax, float2uint8, smooth_data
+from .util import norm_minmax, float2uint8, smooth_data, is_running_jupyter
 
 from . import _version
 __version__ = _version.get_versions()['version']
 
 __all__ = [
+    'is_running_jupyter',
     # gui
     'run_gui',
     # image processing
