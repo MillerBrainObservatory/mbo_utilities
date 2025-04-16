533288cc85822a6b4dfe0de5376e81b343b967a1
Flynn <Flynnoconnell@gmail.com>
2025-04-15 20:35:54 -0400

    init gists

---
+++ gists/util_test.py
@@ -0,0 +1,12 @@
+# /// script
+# requires-python = ">=3.13"
+# dependencies = [
+#     "numpy",
+#     "mbo_utilities",
+#     "fastplotlib",
+# ]
+#
+# [tool.uv.sources]
+# mbo_utilities = { git = "https://github.com/MillerBrainObservatory/mbo_utilities", branch = "master" }
+
+from pathlib import Path
