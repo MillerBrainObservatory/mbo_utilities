b664e8a0751dd5d502aa1f044c9aa9b8821719ff
Flynn OConnell <flynnoconnell@gmail.com>
2025-04-16 22:13:53 -0400

    use new logos and intersphinx

--- docs/conf.py
+++ docs/conf.py
@@ -90,28 +90,28 @@ myst_url_schemes = ("http", "https", "mailto")
 images_config = {"cache_path": "./_images/"}
 
 templates_path = ["_templates"]
+html_static_path = ["_static"]
 
 # A shorter title for the navigation bar.  Default is the same as html_title.
 html_title = "mbo_utilities"
 
-# html_logo = "_static/lcp_logo.svg"
-# html_favicon = "_static/icon_caiman_python.svg"
+html_logo = "./_static/logo_suit2p.png"
+html_favicon = "./_static/icon_suite2p.svg"
 html_theme = "sphinx_book_theme"
 
 html_short_title = "mbo utilities"
-html_static_path = ["_static"]
 html_css_files = ["custom.css"]
 html_copy_source = True
 html_file_suffix = ".html"
-# html_use_modindex = True
+html_use_modindex = True
 
 intersphinx_mapping = {
     "python": ("https://docs.python.org/3.9", None),
     "fastplotlib": ("https://www.fastplotlib.org/", None),
     "numpydoc": ("https://numpydoc.readthedocs.io/en/latest", None),
     "mbo": ("https://millerbrainobservatory.github.io/", None),
-    "utilities": ("https://millerbrainobservatory.github.io/mbo_utilities/", None),
-    "lsp": ("https://millerbrainobservatory.github.io/LBM-Suite2p-Python/", None),
+    "mbo_utilities": ("https://millerbrainobservatory.github.io/mbo_utilities/", None),
+    "lbm_suite2p_python": ("https://millerbrainobservatory.github.io/LBM-Suite2p-Python/", None),
     "caiman": ("https://caiman.readthedocs.io/en/latest/", None),
     "suite2p": ("https://suite2p.readthedocs.io/en/latest/", None),
 }
@@ -133,38 +133,6 @@ html_theme_options = {
             "name": "MBO User Hub",
             "url": "https://millerbrainobservatory.github.io/",
             "icon": "_static/icon_mbo_home.png",
-            "type": "local",
-        },
-        {
-            "name": "MBO Github",
-            "url": "https://github.com/MillerBrainObservatory/",
-            "icon": "fa-brands fa-github",
-            "type": "fontawesome",
-        },
-        {
-            "name": "Connect with MBO",
-            "url": "https://mbo.rockefeller.edu/contact/",
-            "icon": "fa-regular fa-address-card",
-            "type": "fontawesome",
-        },
-    ],
-}
-
-html_theme_options = {
-    "path_to_docs": "docs",
-    "repository_url": "https://github.com/MillerBrainObservatory/mbo_utilties/",
-    "repository_branch": "master",
-    "use_edit_page_button": True,
-    "use_source_button": True,
-    "use_issues_button": True,
-    "use_download_button": True,
-    "show_toc_level": 3,
-    "navbar_align": "content",
-    "icon_links": [
-        {
-            "name": "MBO User Hub",
-            "url": "https://millerbrainobservatory.github.io/",
-            "icon": "./_static/icon_mbo_home.png",
             "type": "local",
         },
         {
