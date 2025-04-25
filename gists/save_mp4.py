66e4eb788dd4cfa7fead581e0b98ffdc5c2486fc
Flynn <Flynnoconnell@gmail.com>
2025-04-25 16:02:30 -0400

    gist: save_mp4, test utils

---
+++ gists/save_mp4.py
@@ -0,0 +1,53 @@
+# /// script
+# requires-python = ">=3.13"
+# dependencies = [
+#     "numpy",
+#     "mbo_utilities",
+#     "fastplotlib",
+# ]
+#
+# [tool.uv.sources]
+# mbo_utilities = { git = "https://github.com/MillerBrainObservatory/mbo_utilities", branch = "master" }
+
+import mbo_utilities as mbo
+from pathlib import Path
+import tifffile
+
+save_path = Path().home().joinpath("dev")
+
+files = mbo.get_files(save_path, 'tif', 4)
+data = tifffile.imread(files[0])
+
+fps = 17
+duration_s = 5
+
+# For each call:
+mbo.save_mp4(
+    save_path.joinpath("default.mp4"),
+    data[:duration_s * fps],  # no speedup
+    framerate=fps,
+)
+
+mbo.save_mp4(
+    save_path.joinpath("speedup_2x.mp4"),
+    data[:duration_s * fps * 2],
+    framerate=fps,
+    speedup=2,
+)
+
+mbo.save_mp4(
+    save_path.joinpath("17_frame_window.mp4"),
+    data[:duration_s * fps],
+    framerate=fps,
+    win=17
+)
+
+mbo.save_mp4(
+    save_path.joinpath("windowed_speedup_6x.mp4"),
+    data[:duration_s * fps * 6],
+    framerate=fps,
+    speedup=6,
+    win=17
+)
+
+x = 2
--- gists/util_test.py
+++ gists/util_test.py
@@ -8,3 +8,21 @@
 #
 # [tool.uv.sources]
 # mbo_utilities = { git = "https://github.com/MillerBrainObservatory/mbo_utilities", branch = "master" }
+
+import mbo_utilities as mbo
+import numpy as np
+from pathlib import Path
+import tifffile
+import fastplotlib as fpl
+
+fname_scan = r"D:\W2_DATA\kbarber\2025_03_01\mk301\green\*"
+save_path = Path().home().joinpath("dev")
+uncor_save = save_path.joinpath("raw")
+corrected_save = save_path.joinpath("corrected")
+
+raw_scan = mbo.read_scan(fname_scan)
+
+mbo.save_as(raw_scan, uncor_save, planes=[7], fix_phase=False)
+mbo.save_as(raw_scan, corrected_save, planes=[7], fix_phase=True)
+
+x = 2
