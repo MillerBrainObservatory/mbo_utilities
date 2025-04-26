0bcffe0503d2cee3ea77faedbd57ac8ceca8617c
Flynn <Flynnoconnell@gmail.com>
2025-04-25 20:33:22 -0400

    add test widget gist

---
+++ gists/test_imgui_widgets.py
@@ -0,0 +1,39 @@
+# /// script
+# requires-python = ">=3.11"
+# dependencies = [
+#     "numpy",
+#     "mbo_utilities",
+#     "lbm_suite2p_python",
+#     "fastplotlib",
+# ]
+#
+# [tool.uv.sources]
+# mbo_utilities = { git = "https://github.com/MillerBrainObservatory/mbo_utilities", branch = "master" }
+# lbm_suite2p_python = { git = "https://github.com/MillerBrainObservatory/lbm_suite2p_python", branch = "master" }
+
+import mbo_utilities as mbo
+import lbm_suite2p_python as lsp
+import numpy as np
+from pathlib import Path
+import tifffile
+import fastplotlib as fpl
+
+
+fpath = r"D:\W2_DATA\kbarber\2025_03_01\mk301\results\plane_07_mk301\plane0"
+save_path = Path().home().joinpath("dev")
+
+# ops = lsp.load_ops(mbo.get_files(fpath, 'ops.npy', 3)[0])
+# raw_path = Path(ops["data_path"][0]) / ops["tiff_list"][0]
+# raw_movie = tifffile.memmap(raw_path)
+
+from fastplotlib.utils import functions
+raw_scan = mbo.read_scan(r"D:\W2_DATA\kbarber\2025_03_01\mk301\green\*")
+
+if __name__ == "__main__":
+    iw = fpl.ImageWidget(data=raw_scan, histogram_widget=True, names=["raw_scan"])
+    gui = mbo.graphics.SummaryDataWidget(image_widget=iw, size=200, location="right")
+    iw.figure.add_gui(gui)
+    iw.show()
+
+    fpl.loop.run()
+
