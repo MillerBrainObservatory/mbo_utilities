d0f74e638a042ff0753f8244ab9d4be9ba2e302c
Flynn OConnell <flynnoconnell@gmail.com>
2025-04-26 13:57:58 -0400

    fix import, tests, addadd imports

--- mbo_utilities/__init__.py
+++ mbo_utilities/__init__.py
@@ -13,7 +13,7 @@ from .file_io import (
 from .assembly import save_as
 from .metadata import is_raw_scanimage, get_metadata, params_from_metadata
 from .image import fix_scan_phase, return_scan_offset
-from .util import norm_minmax, smooth_data, is_running_jupyter
+from .util import (norm_minmax, smooth_data, is_running_jupyter, is_imgui_installed, is_qt_installed)
 
 from .graphics import run_gui
 
