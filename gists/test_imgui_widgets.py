01367597fc59748229dfbcf0ad19b622fb9130b3
Flynn <Flynnoconnell@gmail.com>
2025-04-27 20:52:34 -0400

    few test gists updates

--- gists/test_imgui_widgets.py
+++ gists/test_imgui_widgets.py
@@ -10,25 +10,17 @@
 # [tool.uv.sources]
 # mbo_utilities = { git = "https://github.com/MillerBrainObservatory/mbo_utilities", branch = "master" }
 # lbm_suite2p_python = { git = "https://github.com/MillerBrainObservatory/lbm_suite2p_python", branch = "master" }
-
 import mbo_utilities as mbo
-import lbm_suite2p_python as lsp
-import numpy as np
 from pathlib import Path
-import tifffile
 import fastplotlib as fpl
-from fastplotlib.utils import functions
 
-
-fpath = r"D:\W2_DATA\kbarber\2025_03_01\mk301\results\plane_07_mk301\plane0"
-save_path = Path().home().joinpath("dev")
-
-raw_scan = mbo.read_scan(r"D:\W2_DATA\kbarber\2025_03_01\mk301\green\*")
+raw_scan = mbo.read_scan(r"D://demo//raw_data/*")
+raw_scan[0, 0, :, :]
 
 if __name__ == "__main__":
     iw = fpl.ImageWidget(data=raw_scan, histogram_widget=True, names=["raw_scan"])
-    gui = mbo.graphics.SummaryDataWidget(image_widget=iw, size=200, location="right")
-    iw.figure.add_gui(gui)
+    # gui = mbo.graphics.SummaryDataWidget(image_widget=iw, size=200, location="right")
+    # iw.figure.add_gui(gui)
     iw.show()
 
     fpl.loop.run()
--- gists/tiff_testing.py
+++ gists/tiff_testing.py
@@ -4,13 +4,12 @@
 # ///
 import tifffile, dask.array as da
 import mbo_utilities as mbo
-import fasplotlib as fpl
 
 pathnames = r"D:\demo\raw_data/*"
 filenames = mbo.expand_paths(pathnames)
 scan = mbo.read_scan(filenames)
+scan[0, 0, :, :]
 
-iw = fpl.ImageWindow(scan)
 iw.show()
 
 if __name__ == "__main__":
