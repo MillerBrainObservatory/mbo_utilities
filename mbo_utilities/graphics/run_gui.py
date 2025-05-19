96f0be668c126d74f016ea3d7880cf2b6175ed95
Flynn OConnell <flynnoconnell@gmail.com>
2025-05-19 10:42:54 -0400

    feat: x-platform file_dialog

--- mbo_utilities/graphics/run_gui.py
+++ mbo_utilities/graphics/run_gui.py
@@ -1,14 +1,22 @@
 from pathlib import Path
 import numpy as np
 
-from ..util import is_imgui_installed, is_qt_installed, is_running_jupyter
-from ..file_io import ScanMultiROIReordered, to_lazy_array
+try:
+    from imgui_bundle import portable_file_dialogs as pdf
+except ImportError:
+    MBO_HAS_IMGUI = False
+
+from ..util import is_imgui_installed
+from ..file_io import (
+    ScanMultiROIReordered,
+    to_lazy_array,
+    get_files,
+    read_scan,
+    mbo_home
+)
 
 if is_imgui_installed():
     import fastplotlib as fpl
-if is_qt_installed():
-    from .qt import render_qt_widget
-
 
 def run_gui(
     data_in: None | str | Path | ScanMultiROIReordered | np.ndarray = None, **kwargs
@@ -16,44 +24,17 @@ def run_gui(
     """Open a GUI to preview data."""
     # Handle data_in, which can be a path to files
     if data_in is None:
-        print("No data provided")
-        if not is_qt_installed():
-            raise ValueError(
-                f"No `data_in` argument provided and no qt installation. "
-                f"Support for file loading is only available with Qt installs."
-                f"Install with `pip install -U 'mbo_utilities[all]'`."
-            )
-        else:
-            # set to None, we will load a dialog folder in QT later
-            print("Setting data to None")
-            data = None
+        fd = pdf.select_folder(str(mbo_home))
+        fpath = fd.result()
+        files = get_files(fpath)
+        data_in = read_scan(files)
+    if isinstance(data_in, ScanMultiROIReordered):
+        data = data_in
     else:
-        print("Data provided and set.")
+        data = to_lazy_array(data_in)
 
-        if isinstance(data_in, ScanMultiROIReordered):
-            data = data_in
-        else:
-            data = to_lazy_array(data_in)
-
-    if is_running_jupyter():
-        print("Is running jupyter")
-        # TODO: load dialog when qt isn't installed
-        if data_in is None:
-            print("Running jupyter, no data provided")
-            if not is_qt_installed():
-                raise ValueError(
-                    f"No `data_in` argument provided and no qt installation. "
-                    f"Support for file loading is only available with Qt installs."
-                    f"Install with `pip install -U 'mbo_utilities[all]'`."
-                )
-            return None
-        else:
-            # no data, in Jupyter, we need a QT dialog to load a data path
-            # can remove this once we have a means to load a native file-dialog from within jupyter
-            iw = fpl.ImageWidget(data=data, histogram_widget=True, **kwargs)
-            iw.show()
-            return iw
-    else:  # not runniing jupyter
-        print(f"Not running Jupyter, Rendering qt widget")
-        render_qt_widget(data=data)
-        return None
+    iw = fpl.ImageWidget(data=data, histogram_widget=True, **kwargs)
+    iw.figure.add_gui()
+    iw.show()
+    fpl.loop.run()
+    return None
