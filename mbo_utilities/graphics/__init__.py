035e83f2f351e356b11333da4b7e042ee2cbefcb
Flynn OConnell <flynnoconnell@gmail.com>
2025-05-20 23:02:33 -0400

    chore: clean unused funcs and imports

--- mbo_utilities/graphics/__init__.py
+++ mbo_utilities/graphics/__init__.py
@@ -1,30 +1,20 @@
-from ..util import is_qt_installed, is_imgui_installed
+from ..util import is_imgui_installed
 
 __all__ = []
-
-if is_qt_installed():
-    HAS_QT = True
-else:
-    HAS_QT = False
-    load_dialog_folder = None
-    LBMMainWindow = None
 
 if is_imgui_installed():
     HAS_IMGUI = True
     from .imgui import SummaryDataWidget, PreviewDataWidget
 else:
     HAS_IMGUI = False
-    PollenCalibrationWidget = None
+    PreviewDataWidget = None
     SummaryDataWidget = None
 
 from .run_gui import run_gui
 
 __all__ += [
-    "load_dialog_folder",
-    "LBMMainWindow",
-    "HAS_QT",
     "SummaryDataWidget",
-    "PollenCalibrationWidget",
+    "PreviewDataWidget",
     "HAS_IMGUI",
     "run_gui",
 ]
