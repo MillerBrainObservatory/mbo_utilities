4a2a10709478296495649332fd97e396ffd7b74d
Flynn <Flynnoconnell@gmail.com>
2025-05-20 21:21:49 -0400

    temp gist update

--- gists/save_as.py
+++ gists/save_as.py
@@ -8,6 +8,7 @@
 #
 # [tool.uv.sources]
 # mbo_utilities = { git = "https://github.com/MillerBrainObservatory/mbo_utilities", branch = "master" }
+import tifffile
 
 import mbo_utilities as mbo
 from pathlib import Path
@@ -16,36 +17,10 @@ from pprint import pprint
 
 if __name__ == "__main__":
     # raw = Path(r"D://demo//raw")
-    raw = Path(r"/home/flynn/lbm_data/raw")
+    raw = Path(r"D:\W2_DATA\kbarber\2025_03_01\mk301\green")
     test_scan = mbo.read_scan(raw, roi=2)
-    # savedir = r"D://demo//assembled_ofs"
-    savedir = r"/home/flynn/lbm_data/roi_1"
-    savedir = r"/home/flynn/lbm_data/roi_2"
-    mbo.save_as(test_scan, savedir, ext=".tiff", overwrite=False, fix_phase=True, planes=[6], debug=True)
-    files = mbo.get_files(savedir, 'tif')
-    metadata = mbo.get_metadata(files[0])
-    pprint(metadata)
-    len(files)
-    # save_path = Path().home().joinpath("lbm_data/output")
-    # mbo.save_as(scan, save_path, [6, 13], ext=".bin", overwrite=False)
-    # data = np.memmap(save_path.joinpath("plane0/raw_data.bin"), dtype="int16").reshape(
-    #     (1437, 448, 448)
-    # )
-    # data2 = np.memmap(save_path.joinpath("plane6/raw_data.bin"), dtype="int16").reshape(
-    #     (1437, 448, 448)
-    # )
-    # data3 = np.memmap(save_path.joinpath("plane9/raw_data.bin"), dtype="int16").reshape(
-    #     (1437, 448, 448)
-    # )
-    # data4 = np.memmap(
-    #     save_path.joinpath("plane13/raw_data.bin"), dtype="int16"
-    # ).reshape((1437, 448, 448))
-    # iw = fpl.ImageWidget(
-    #     data=[data, data2, data3, data4],
-    #     names=["Plane 1", "Plane 2", "Plane 3", "Plane 4"],
-    #     histogram_widget=False,
-    # )
-    # for subplot in iw.figure:
-    #     subplot.toolbar = False
-    # iw.show()
-    # fpl.loop.run()
+    savedir = r"D:\W2_DATA\kbarber\2025_03_01\mk301\masknmf\roi_2"
+    mbo.save_as(test_scan, savedir, ext=".tiff", overwrite=True, fix_phase=True, planes=[7], debug=False)
+    files = list(Path(savedir).glob("*.tif*"))[0]
+    img = tifffile.imread(files)
+    print(img.shape)
