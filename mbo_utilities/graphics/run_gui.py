3de4a703aed454b2ba9703a802f453150e947c18
Flynn OConnell <flynnoconnell@gmail.com>
2025-05-21 00:31:19 -0400

    fix run_gui for 2D/3D cases

--- mbo_utilities/graphics/run_gui.py
+++ mbo_utilities/graphics/run_gui.py
@@ -10,6 +10,7 @@ from ..util import is_imgui_installed
 from .imgui import PreviewDataWidget
 from ..file_io import (
     ScanMultiROIReordered,
+    _is_arraylike,
     to_lazy_array,
     get_files,
     read_scan,
@@ -19,45 +20,62 @@ from ..file_io import (
 if is_imgui_installed():
     import fastplotlib as fpl
 
+from pathlib import Path
+import numpy as np
 
-def run_gui(
-    data_in: None | str | Path | ScanMultiROIReordered | np.ndarray = None, **kwargs
-):
-    """Open a GUI to preview data."""
-    # Handle data_in, which can be a path to files
+try:
+    from imgui_bundle import portable_file_dialogs as pdf
+except ImportError:
+    MBO_HAS_IMGUI = False
+
+from ..util import is_imgui_installed
+from .imgui import PreviewDataWidget
+from ..file_io import (
+    to_lazy_array,
+    get_files,
+    read_scan,
+    mbo_home
+)
+
+if is_imgui_installed():
+    import fastplotlib as fpl
+
+
+def run_gui(data_in=None, **kwargs):
+    """Open a GUI to preview data of any supported type."""
+
     if data_in is None:
         fd = pdf.select_folder(str(mbo_home))
-        fpath = fd.result()
+        fpath = Path(fd.result())
         files = get_files(fpath)
-        data_in = read_scan(files)
-    if isinstance(data_in, ScanMultiROIReordered):
+        data = read_scan(files)
+    elif _is_arraylike(data_in):
         data = data_in
     else:
-        # all logic for what this function can support is handled here
         data = to_lazy_array(data_in)
 
-    if isinstance(data_in, list):
-        vmin, vmax = data[0].min, data[0].max
-        nx, ny = data[0].shape[-2:]
+    if isinstance(data, list):
+        sample = data[0]
     else:
-        vmin, vmax = data.min, data.max
-        nx, ny = data.shape[-2:]
+        sample = data
 
+    if sample.ndim < 2:
+        raise ValueError(f"Invalid input shape: expected >=2D, got {sample.shape}")
+
+    vmin, vmax = sample.min(), sample.max()
+    nx, ny = sample.shape[-2:]
     iw = fpl.ImageWidget(
         data=data,
         histogram_widget=False,
-        figure_kwargs={
-            "size": (nx, ny),
-        },
+        figure_kwargs={"size": (nx, ny)},
         graphic_kwargs={"vmin": vmin, "vmax": vmax},
         window_funcs={"t": (np.mean, 0)},
     )
 
-    add_gui = kwargs.get("gui", None)
-    if add_gui:
+    if kwargs.get("gui"):
         gui = PreviewDataWidget(iw=iw)
         iw.figure.add_gui(gui)
 
     iw.show()
     fpl.loop.run()
-    return None
+
