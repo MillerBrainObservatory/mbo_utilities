af53da4cc28cc1af7c7d6bbee7d3bf8738063279
Flynn OConnell <flynnoconnell@gmail.com>
2025-05-21 22:49:02 -0400

    feat: preview data widget, assets

--- mbo_utilities/graphics/__init__.py
+++ mbo_utilities/graphics/__init__.py
@@ -1,20 +1,8 @@
-from ..util import is_imgui_installed
-
-__all__ = []
-
-if is_imgui_installed():
-    HAS_IMGUI = True
-    from .imgui import SummaryDataWidget, PreviewDataWidget
-else:
-    HAS_IMGUI = False
-    PreviewDataWidget = None
-    SummaryDataWidget = None
-
 from .run_gui import run_gui
+from .imgui import SummaryDataWidget, PreviewDataWidget
 
-__all__ += [
+__all__ = [
     "SummaryDataWidget",
     "PreviewDataWidget",
-    "HAS_IMGUI",
     "run_gui",
 ]
