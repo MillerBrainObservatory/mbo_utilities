7718122f813fda059ff9f72b8d9efdb83f418524
Flynn <Flynnoconnell@gmail.com>
2025-05-26 18:21:39 -0400

    fonts, pipeline selector, assets/ini setup with fd

--- mbo_utilities/graphics/_file_dialog.py
+++ mbo_utilities/graphics/_file_dialog.py
@@ -1,29 +1,16 @@
-import shutil
-from pathlib import Path
-from typing import List
-
-from icecream import ic
 from imgui_bundle import (
     imgui,
     imgui_md,
     hello_imgui,
 )
 from imgui_bundle import portable_file_dialogs as pfd
-import os
 
 
 class FileDialog:
     def __init__(self):
-        self.open_file_dialog = None
-        self.open_file_multiselect = None
-        self.save_file_dialog = None
-        self.select_folder_dialog = None
-        self.last_file_selection = ""
         self.selected_path = None
-        self.last_file_selection = ""
-        self.icon_type = pfd.icon.info
-        self.message_dialog = None
-        self.message_choice_type = pfd.choice.ok
+        self._open_multi = None
+        self._select_folder = None
 
     def render(self):
         imgui.push_id("pfd")
@@ -34,64 +21,19 @@ class FileDialog:
         [mbo_utilities](https://github.com/MillerBrainObservatory/mbo_utilities/tree/subpixel-phasecorr) 
         [docs](https://millerbrainobservatory.github.io/mbo_utilities/) 
         """)
-
-        imgui.text("      ---   File dialogs   ---")
-        if imgui.button("Open file"):
-            self.open_file_dialog = pfd.open_file("Select file")
-        if self.open_file_dialog and self.open_file_dialog.ready():
-            self.selected_path = self.open_file_dialog.result()
-            self.open_file_dialog = None
-            # continue other processing here
-
-        imgui.same_line()
         if imgui.button("Open file (multiselect)"):
-            self.open_file_multiselect = pfd.open_file(
-                "Select file", options=pfd.opt.multiselect
-            )
-        if self.open_file_multiselect and self.open_file_multiselect.ready():
-            self.selected_path = self.open_file_multiselect.result()
-            self.open_file_multiselect = None
+            self._open_multi = pfd.open_file("Select files", options=pfd.opt.multiselect)
+        if self._open_multi and self._open_multi.ready():
+            self.selected_path = self._open_multi.result()
+            self._open_multi = None
+            hello_imgui.get_runner_params().app_shall_exit = True
 
         imgui.same_line()
         if imgui.button("Select folder"):
-            self.select_folder_dialog = pfd.select_folder("Select folder")
-        if self.select_folder_dialog and self.select_folder_dialog.ready():
-            self.selected_path = self.select_folder_dialog.result()
-            self.select_folder_dialog = None
-
-        if self.selected_path:
-            imgui.text(self.last_file_selection)
+            self._select_folder = pfd.select_folder("Select folder")
+        if self._select_folder and self._select_folder.ready():
+            self.selected_path = self._select_folder.result()
+            self._select_folder = None
             hello_imgui.get_runner_params().app_shall_exit = True
 
         imgui.pop_id()
-
-
-fd = FileDialog()
-
-
-def render_file_dialog():
-    global fd
-    fd.render()
-
-
-if __name__ == "__main__":
-    from imgui_bundle.demos_python import demo_utils
-
-    demo_utils.set_hello_imgui_demo_assets_folder()
-
-    from imgui_bundle import immapp
-    from imgui_bundle import hello_imgui
-
-    imgui_ini_path = os.path.expanduser("~/.mbo/imgui.ini")
-    os.makedirs(os.path.dirname(imgui_ini_path), exist_ok=True)
-
-    runner_params = hello_imgui.RunnerParams()
-    loc = hello_imgui.ini_settings_location(hello_imgui.IniFolderType.home_folder)
-    runner_params.ini_filename = imgui_ini_path
-    runner_params.callbacks.show_gui = render_file_dialog
-
-    hello_imgui.run(runner_params)
-
-    immapp.run(render_file_dialog, with_markdown=True, window_size=(500, 500))  # type: ignore
-    if fd.selected_path:
-        print(fd.selected_path)
--- mbo_utilities/graphics/run_gui.py
+++ mbo_utilities/graphics/run_gui.py
@@ -1,43 +1,76 @@
+import shutil
+from pathlib import Path
+
 import click
 import numpy as np
+from icecream import ic
 
-from mbo_utilities.util import is_imgui_installed
 from mbo_utilities.graphics.imgui import PreviewDataWidget
 from mbo_utilities.file_io import (
     to_lazy_array,
-    get_files,
-    read_scan,
     _is_arraylike,
+    _get_mbo_dirs, _get_mbo_project_root,
 )
 import fastplotlib as fpl
-from imgui_bundle import immapp
-from mbo_utilities.graphics._file_dialog import render_file_dialog
+from imgui_bundle import immapp, imgui, hello_imgui
+from mbo_utilities.graphics._file_dialog import FileDialog
 
-global selected
+def setup():
+    project_assets: Path = _get_mbo_project_root().joinpath("assets")
+    mbo_dirs = _get_mbo_dirs()
+
+    imgui_path = mbo_dirs["base"].joinpath("imgui")
+    imgui_path.mkdir(exist_ok=True)
+
+    imgui_ini_path = imgui_path.joinpath("imgui.ini")
+    imgui_ini_path.parent.mkdir(exist_ok=True)
+    imgui.create_context()
+    imgui.get_io().set_ini_filename(str(imgui_ini_path))
+
+    if not project_assets.is_dir():
+        ic("Assets folder not found.")
+        return
+
+    assets_path = imgui_path.joinpath("assets")
+    assets_path.mkdir(exist_ok=True)
+
+    shutil.copytree(project_assets, assets_path, dirs_exist_ok=True)
+    hello_imgui.set_assets_folder(str(project_assets))
+
+    font_path = assets_path / "fonts" / "JetBrainsMono-Bold.ttf"
+    if font_path.is_file():
+        imgui.get_io().fonts.clear()
+        imgui.get_io().fonts.add_font_from_file_ttf(str(font_path), 16.0)
+    else:
+        ic("Font not found:", font_path)
+
+
+setup()
 
 
 @click.command()
-@click.option("--roi", "-r", type=click.IntRange(1, 10), default=None)
+@click.option("--roi", type=click.IntRange(1, 10), default=None)
 @click.option(
-    "--gui/--no-gui",
-    default=None,
-    help="Enable or disable PreviewDataWidget. Default is auto.",
+    "--widget",
+    default=True,
+    help="Enable or disable PreviewDataWidget. Default is enabled.",
 )
 @click.argument("data_in", required=False)
-def run_gui(data_in=None, gui=None, roi=None, **kwargs):
+def run_gui(data_in=None, widget=None, roi=None, **kwargs):
     """Open a GUI to preview data of any supported type."""
     if data_in is None:
+        file_dialog = FileDialog()
+        def render_file_dialog():
+            x = hello_imgui.RunnerParams.ini_filename
+            file_dialog.render()
         immapp.run(render_file_dialog, with_markdown=True, window_size=(1000, 1000))  # type: ignore
-        if not selected:
-            print("not selected")
-        else:
-            fpath = selected
-        files = get_files(fpath)
-        data = read_scan(files)
-    elif _is_arraylike(data_in):
+        data_in = file_dialog.selected_path
+    if _is_arraylike(data_in):
+        ic(data_in)
         data = data_in
     else:
-        data = to_lazy_array(data_in)
+        ic(data_in)
+        data = to_lazy_array(data_in, roi=roi, **kwargs)
 
     if isinstance(data, list):
         sample = data[0]
@@ -51,14 +84,18 @@ def run_gui(data_in=None, gui=None, roi=None, **kwargs):
     iw = fpl.ImageWidget(
         data=data,
         histogram_widget=False,
-        figure_kwargs={"size": (nx, ny)},
+        figure_kwargs={"size": (nx*2.5, ny*2.5)},
         graphic_kwargs={"vmin": sample.min(), "vmax": sample.max()},
         window_funcs={"t": (np.mean, 0)},
     )
 
-    if kwargs.get("gui"):
-        gui = PreviewDataWidget(iw=iw)
+    if widget:
+        fpath = data.fpath if hasattr(data, "fpath") else None
+        gui = PreviewDataWidget(iw=iw, fpath=fpath)
         iw.figure.add_gui(gui)
 
     iw.show()
     fpl.loop.run()
+
+if __name__ == "__main__":
+    run_gui()  # type: ignore
