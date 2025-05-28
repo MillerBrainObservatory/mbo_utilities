5d6a2218b70642916ab869a9d8cf299fd8acc759
Flynn OConnell <flynnoconnell@gmail.com>
2025-05-28 13:39:46 -0400

    update pipelines, dataclass storage for vars, fix groups

--- mbo_utilities/graphics/_imgui.py
+++ mbo_utilities/graphics/_imgui.py
@@ -9,7 +9,6 @@ from mbo_utilities.file_io import (
     _get_mbo_dirs,
 )
 
-
 def setup_imgui():
     project_assets: Path = _get_mbo_project_root().joinpath("assets")
     mbo_dirs = _get_mbo_dirs()
