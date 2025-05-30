d4a57606c5c8b761837b1be0d64e7fe8498ec209
Flynn <Flynnoconnell@gmail.com>
2025-05-30 15:49:44 -0400

    ci: save full fov and rois, compare outputs without scan-phase correction

--- gists/tiff_testing.py
+++ gists/tiff_testing.py
@@ -2,11 +2,9 @@
 # requires-python = ">=3.13"
 # dependencies = ["click", "numpy", "tifffile", "tqdm", "dask[complete]", "fastplotlib[notebook, imgui]"]
 # ///
-import tifffile, dask.array as da
-import mbo_utilities as mbo
 from mbo_utilities.lazy_array import LazyArrayLoader
 
 if __name__ == "__main__":
-    pathnames = r"D:\tests\data"
+    pathnames = r"D:\demo\assembled"
     lazy_array = LazyArrayLoader(pathnames).load()
     print(lazy_array)
