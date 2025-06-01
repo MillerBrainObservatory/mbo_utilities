ed0ef4c142122f729e74fc99d5475cb240fbaeb9
Flynn OConnell <flynnoconnell@gmail.com>
2025-05-31 23:40:38 -0400

    dev scripts

--- gists/save_as.py
+++ gists/save_as.py
@@ -8,14 +8,32 @@
 #
 # [tool.uv.sources]
 # mbo_utilities = { git = "https://github.com/MillerBrainObservatory/mbo_utilities", branch = "dev" }
+import mbo_utilities as mbo
 import tifffile
 
-import mbo_utilities as mbo
-from pathlib import Path
+from mbo_utilities import is_raw_scanimage
+from mbo_utilities.metadata import has_mbo_metadata
+
+
+def find_si_rois(file):
+    """
+    Find the ROIs in the current ScanImage session.
+
+    Returns
+    -------
+    list
+        List of ROI names.
+    """
+    with tifffile.TiffFile(file, mode="r") as _tf:
+        if is_raw_scanimage(file):
+            si_metadata = _tf.scanimage_metadata
+        if has_mbo_metadata(file):
+            si_metadata = _tf.shaped_metadata[0]["si"]
+        rois = si_metadata["RoiGroups"]["imagingRoiGroup"]["rois"]
+    return rois
+
 
 if __name__ == "__main__":
-    raw = Path(r"")
-
     test_scan = mbo.read_scan("/home/flynn/lbm_data/raw", roi=0)
     savedir = r"/home/flynn/lbm_data/saved"
     mbo.save_as(
