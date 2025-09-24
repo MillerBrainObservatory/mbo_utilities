d8ed87cdfe9005fabce10f4b4740ee4df668a002
Flynn <Flynnoconnell@gmail.com>
2025-09-24 13:35:44 -0400

    remove extra print statements

--- mbo_utilities/metadata.py
+++ mbo_utilities/metadata.py
@@ -376,12 +376,10 @@ def get_metadata_single(file: os.PathLike | str, z_step=None, verbose=False):
     """
     tiff_file = tifffile.TiffFile(file)
     if not is_raw_scanimage(file):
-        print(f"File {file} is not a raw ScanImage TIFF. Attempting to read saved metadata.")
         if not hasattr(tiff_file, "shaped_metadata") or tiff_file.shaped_metadata is None:
             raise ValueError(f"No metadata found in {file}.")
         return tiff_file.shaped_metadata[0]
     elif hasattr(tiff_file, "scanimage_metadata"):
-        print("Raw ScanImage TIFF detected. Extracting metadata...")
         meta = tiff_file.scanimage_metadata
         if meta is None:
             return None
