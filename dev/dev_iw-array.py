631103d30098677ba3824593397e49c650ccb3f8
Flynn <Flynnoconnell@gmail.com>
2025-11-22 13:28:15 -0500

    dev nbs, cleanup

---
+++ dev/dev_iw-array.py
@@ -0,0 +1,17 @@
+from pathlib import Path
+from mbo_utilities.graphics.run_gui import run_gui
+from mbo_utilities import imread
+import fastplotlib as fpl
+
+# Initial data path
+data_path = r"E:\tests\lbm\mbo_utilities\big_raw"
+data = imread(data_path)
+print(f"Initial data shape: {data.shape}")
+
+# Create ImageWidget
+pdw = run_gui(data)
+# iw = fpl.ImageWidget(
+#     [data],
+#     names=["My Custom Filetype"],
+#     slider_dim_names=("t", "z"),
+# )
