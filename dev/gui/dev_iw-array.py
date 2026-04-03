170bc0b5ed3122bcfa98ac4927752458e8e903c9
Flynn <75346097+FlynnOConnell@users.noreply.github.com>
2026-04-03 10:09:53 -0700

    bump v2.7.0 (#163)

--- dev/gui/dev_iw-array.py
+++ dev/gui/dev_iw-array.py
@@ -1,15 +1,16 @@
 # import time
 # from pathlib import Path
-# from mbo_utilities.graphics.run_gui import run_gui
+from mbo_utilities.gui.run_gui import run_gui
 import mbo_utilities
 # import fastplotlib as fpl
-# from mbo_utilities import subsample_array
+# from mbo_utilities import run_gui
 
 # Initial data
-data_path = r"D:/demo/raw"
+data_path = r"E:\datasets\pollen\2026-03-31_thorlabs-obj-testing\Pollen_4p1x_5um-Step_Thorlabs28x"
 data = mbo_utilities.imread(data_path)
 print(data.metadata)
-# pdw = run_gui(data)
+pdw = run_gui(data)
+
 # iw = fpl.ImageWidget(
 #     [data],
 #     names=["My Custom Filetype"],
